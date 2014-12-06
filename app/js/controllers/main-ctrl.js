7f4a3a11647162535ca001b0bfbf31481b86f162
Russell Wenban <russell@factornine.co.uk>
2014-12-06 16:27:57 +0000

    Added app with grid view

--- app/js/controllers/main-ctrl.js
+++ app/js/controllers/main-ctrl.js
@@ -1,12 +1,41 @@
 'use strict';
 
-controllerModule.controller('MainCtrl', function($scope, $timeout) {
-  $scope.demo = 'circles';
+controllerModule.controller('MainCtrl', function ($scope, $famous) {
+    var Engine = $famous['famous/core/Engine'];
+    var EventHandler = $famous['famous/core/EventHandler'];
 
-  $scope.switchDemo = function(demo) {
-    $scope.demo = demo;
-    $scope.demoTemplate = 'animations/_' + demo + '.html';
-  };
+    $scope.enginePipe = new EventHandler();
+    Engine.pipe($scope.enginePipe);
 
-  $scope.switchDemo('circles');
+    $scope.gridElements = [
+        {backgroundColor: '#F00', index: 1},
+        {backgroundColor: '#FF0', index: 2},
+        {backgroundColor: '#0F0', index: 3},
+        {backgroundColor: '#0FF', index: 4},
+        {backgroundColor: '#00F', index: 5},
+        {backgroundColor: '#F0F', index: 6},
+        {backgroundColor: '#F00', index: 7},
+        {backgroundColor: '#FF0', index: 8},
+        {backgroundColor: '#0F0', index: 9},
+        {backgroundColor: '#0FF', index: 10},
+        {backgroundColor: '#00F', index: 11},
+        {backgroundColor: '#F0F', index: 12}
+    ];
+
+    $scope.enginePipe.on('click', function(){
+        //$scope.changeDimensions();
+        //$scope.gridElements.splice(0, 1);
+
+        //if(!$scope.$$phase) $scope.$apply();
+    });
+
+    $scope.gridLayoutOptions = {
+        dimensions: [3, 3]
+    };
+
+    $scope.changeDimensions = function(){
+        console.log('changing dimensions')
+        $scope.gridLayoutOptions.dimensions[0]++;
+    }
+
 });
