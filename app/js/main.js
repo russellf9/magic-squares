647da59d22aa15cba333e8488a22e09767363add
russellf9 <russell@factornine.co.uk>
2015-01-27 15:50:47 +0000

    * Re-factoring  - sorted the dependancies.

--- app/js/main.js
+++ app/js/main.js
@@ -23,7 +23,7 @@
         }])
 // I provide an injectable (and exteded) version of the underscore / lodash lib.
         .factory(
-        '_',
+        '_',['$window',
         function($window) {
             // Get a local handle on the global lodash reference.
             var _ = $window._;
@@ -81,7 +81,7 @@
             // Return the [formerly global] reference so that it can be injected
             // into other aspects of the AngularJS application.
             return (_);
-        }
+        }]
     );
 }());
 
